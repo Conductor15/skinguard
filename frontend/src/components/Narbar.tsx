1d901be4c026eab4123a7ff1de479fc550d76d1d
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2025-04-13 00:06:45 +0700

    first navbar

--- frontend/src/components/Narbar.tsx
+++ frontend/src/components/Narbar.tsx
@@ -3,14 +3,21 @@ import '../assets/styles.css'
 const Narbar = () => {
   return (
     <div className='navbar'>
-        <img src={TechwiqLogo} alt="Techwiq Logo" width={36} height={60}/>
-        <div className='name_Logo'>
-          <span className='skin_Logo'>
-            SKIN
-          </span>
-          <span className='guard_Logo'>
-            GUARD
-          </span>
+        <div className='left_Navbar'>
+          <img src={TechwiqLogo} alt="Techwiq Logo" width={36} height={60}/>
+          <div className='name_Logo'>
+            <span className='skin_Logo'>
+              SKIN
+            </span>
+            <span className='guard_Logo'>
+              GUARD
+            </span>
+          </div>
+        </div>
+        
+        <div className='right_Navbar'>
+          <span className='first_Letter_right_Navbar'>We unveil what the skin conceals—early</span>
+          <span className='second_Letter_right_Navbar'>diagnosis through the lens of technology</span>
         </div>
     </div>
   )
