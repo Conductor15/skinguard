8798d16a1491ac430b569368178034cb049ee732
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2025-04-15 13:09:03 +0700

    add content_2

--- FrontEnd/src/components/content_2.tsx
+++ FrontEnd/src/components/content_2.tsx
@@ -1,21 +1,80 @@
-import '../assets/content_2.css'
+import '../assets/content_2.css';
 
 const Content_2 = () => {
+    const features = [
+        {
+            icon: (
+                <svg viewBox="0 0 91 91" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+                            <g>
+                                <path d="M45.527,1.506c-24.514,0-44.459,19.945-44.459,44.463c0,24.519,19.945,44.464,44.459,44.464
+                                c24.52,0,44.467-19.945,44.467-44.464C89.994,21.452,70.047,1.506,45.527,1.506z" fill="#647F94" />
+                                <path d="M22.791,45.995c0-1.385-1.123-2.506-2.506-2.506h-7.051c-1.383,0-2.506,1.121-2.506,2.506
+                                s1.123,2.504,2.506,2.504h7.051C21.668,48.499,22.791,47.38,22.791,45.995z" fill="#95AEC2" />
+                                <path d="M25.24,30.726c0.479,0.438,1.084,0.654,1.689,0.654c0.678,0,1.355-0.273,1.85-0.814
+                                c0.934-1.021,0.861-2.607-0.16-3.54l-5.205-4.76c-1.018-0.932-2.605-0.863-3.539,0.158c-0.934,1.023-0.861,2.607,0.16,3.541L25.24,30.726z"
+                                fill="#95AEC2" />
+                                <path d="M62.596,29.778c0.68,0,1.355-0.273,1.85-0.814l4.756-5.206c0.934-1.021,0.861-2.605-0.16-3.539
+                                c-1.02-0.93-2.604-0.861-3.539,0.16l-4.754,5.205c-0.934,1.02-0.861,2.606,0.158,3.54C61.389,29.563,61.994,29.778,62.596,29.778z"
+                                fill="#95AEC2" />
+                                <path d="M26.68,62.854l-4.762,5.201c-0.934,1.02-0.863,2.605,0.156,3.539c0.482,0.439,1.086,0.658,1.691,0.658
+                                c0.678,0,1.354-0.273,1.848-0.816l4.762-5.199c0.936-1.02,0.865-2.605-0.156-3.539C29.201,61.764,27.617,61.83,26.68,62.854z"
+                                fill="#95AEC2" />
+                                <path d="M65.887,61.094c-1.018-0.93-2.604-0.861-3.537,0.16c-0.934,1.02-0.861,2.605,0.158,3.539l5.207,4.758
+                                c0.479,0.439,1.084,0.656,1.689,0.656c0.678,0,1.355-0.273,1.85-0.816c0.932-1.02,0.861-2.605-0.16-3.539L65.887,61.094z"
+                                fill="#95AEC2" />
+                                <path d="M45.666,68.547c-1.383,0-2.504,1.121-2.504,2.504v7.158c0,1.385,1.121,2.506,2.504,2.506
+                                s2.506-1.121,2.506-2.506v-7.158C48.172,69.668,47.049,68.547,45.666,68.547z" fill="#95AEC2" />
+                                <path d="M77.828,43.489h-7.049c-1.383,0-2.504,1.121-2.504,2.506s1.121,2.504,2.504,2.504h7.049
+                                c1.383,0,2.506-1.119,2.506-2.504S79.211,43.489,77.828,43.489z" fill="#95AEC2" />
+                                <path d="M63.053,45.995c0-1.385-1.121-2.506-2.504-2.506H48.172V13.731c0-1.383-1.123-2.506-2.506-2.506
+                                s-2.504,1.123-2.504,2.506v32.264c0,1.385,1.121,2.504,2.504,2.504h14.883C61.932,48.499,63.053,47.38,63.053,45.995z"
+                                fill="#6EC4A7" />
+                            </g>
+                        </svg>
+            ),
+            text: "Fast and Convenient"
+        },
+        {
+            icon: (
+                <svg viewBox="0 0 1024 1024" className="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M951.87 253.86c0-82.18-110.05-144.14-256-144.14s-256 61.96-256 144.14c0 0.73 0.16 1.42 0.18 2.14h-0.18v109.71h73.14v-9.06c45.77 25.81 109.81 41.33 182.86 41.33 67.39 0 126.93-13.33 171.71-35.64 6.94 7.18 11.15 14.32 11.15 20.58 0 28.25-72.93 70.98-182.86 70.98h-73.12v73.14h73.12c67.4 0 126.96-13.33 171.74-35.65 6.95 7.17 11.11 14.31 11.11 20.6 0 28.27-72.93 71-182.86 71l-25.89 0.12c-15.91 0.14-31.32 0.29-46.34-0.11l-1.79 73.11c8.04 0.2 16.18 0.27 24.48 0.27 7.93 0 16-0.05 24.2-0.12l25.34-0.12c67.44 0 127.02-13.35 171.81-35.69 6.97 7.23 11.04 14.41 11.04 20.62 0 28.27-72.93 71-182.86 71h-73.12v73.14h73.12c67.44 0 127.01-13.35 171.81-35.69 6.98 7.22 11.05 14.4 11.05 20.62 0 28.27-72.93 71-182.86 71h-73.12v73.14h73.12c145.95 0 256-61.96 256-144.14 0-0.68-0.09-1.45-0.11-2.14h0.11V256h-0.18c0.03-0.72 0.2-1.42 0.2-2.14z m-438.86 0c0-28.27 72.93-71 182.86-71s182.86 42.73 182.86 71c0 28.25-72.93 70.98-182.86 70.98s-182.86-42.73-182.86-70.98z" fill="#F8C245"></path><path d="M330.15 365.71c-145.95 0-256 61.96-256 144.14 0 0.73 0.16 1.42 0.18 2.14h-0.18v256c0 82.18 110.05 144.14 256 144.14s256-61.96 256-144.14V512h-0.18c0.02-0.72 0.18-1.42 0.18-2.14 0-82.18-110.05-144.15-256-144.15zM147.29 638.93c0-6.32 4.13-13.45 11.08-20.62 44.79 22.33 104.36 35.67 171.78 35.67 67.39 0 126.93-13.33 171.71-35.64 6.94 7.18 11.15 14.32 11.15 20.58 0 28.25-72.93 70.98-182.86 70.98s-182.86-42.72-182.86-70.97z m182.86-200.07c109.93 0 182.86 42.73 182.86 71 0 28.25-72.93 70.98-182.86 70.98s-182.86-42.73-182.86-70.98c0-28.27 72.93-71 182.86-71z m0 400.14c-109.93 0-182.86-42.73-182.86-71 0-6.29 4.17-13.43 11.11-20.6 44.79 22.32 104.34 35.66 171.75 35.66 67.4 0 126.96-13.33 171.74-35.65 6.95 7.17 11.11 14.31 11.11 20.6 0.01 28.26-72.92 70.99-182.85 70.99z" fill="#F8C245"></path></g></svg>
+            ),
+            text: "100% Free and Always Available"
+        },
+        {
+            icon: (
+                <svg viewBox="0 0 1024 1024" className="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M558.4 511.9v-31.2c0-4.7-3.8-8.5-8.5-8.5h-56.4c-4.7 0-8.5 3.8-8.5 8.5v27.9c0 12.9-10.5 23.4-23.4 23.4h-26.9c-4.7 0-8.5 3.8-8.5 8.5v56.4c0 4.7 3.8 8.5 8.5 8.5h16.1c5.5 0 14.2 4.5 14.2 10l1 41.7c0 10.3 3.1 18.6 13.4 18.6h13.1c4.2 0 8.1 1.4 11.2-0.6 0.4-0.2 0.8-0.4 1.2-0.4h41.7c6.4 0 11.7-5.2 11.7-11.7v-35.6c0-12.2 9.8-22 22-22H617c4.7 0 8.5-3.8 8.5-8.5v-66.8c0-8.9-7.2-16.1-16.1-16.1h-48.8c-1.2 0.1-2.2-0.9-2.2-2.1z" fill="#ACE2F9"></path><path d="M855.4 289.3c15.1 0 29.4 6 40.2 16.8s16.8 25.1 16.8 40.2V750c0 15.1-6 29.4-16.8 40.2-10.7 10.8-25 16.8-40.2 16.8H170c-15.1 0-29.4-6-40.2-16.8S113 765.1 113 750V346.3c0-15.1 6-29.4 16.8-40.2 10.8-10.8 25.1-16.8 40.2-16.8h685.4m0-15H170c-39.6 0-72 32.4-72 72V750c0 39.6 32.4 72 72 72h685.4c39.6 0 72-32.4 72-72V346.3c0-39.6-32.4-72-72-72z" fill="#999999"></path><path d="M630.7 213.3v98.8h-236v-98.8h236m3-15h-242c-6.6 0-12 5.4-12 12v104.8c0 6.6 5.4 12 12 12h242c6.6 0 12-5.4 12-12V210.3c0-6.6-5.4-12-12-12zM275.3 259.3V274h-61.7v-14.7h61.7m3-15h-67.7c-6.6 0-12 5.4-12 12V277c0 6.6 5.4 12 12 12h67.7c6.6 0 12-5.4 12-12v-20.7c0-6.6-5.4-12-12-12zM811.8 259.3V274h-61.7v-14.7h61.7m3-15h-67.7c-6.6 0-12 5.4-12 12V277c0 6.6 5.4 12 12 12h67.7c6.6 0 12-5.4 12-12v-20.7c0-6.6-5.4-12-12-12zM513.3 409.4c40.8 0 79.1 15.9 108 44.7 28.8 28.8 44.7 67.2 44.7 108s-15.9 79.1-44.7 108c-28.8 28.8-67.2 44.7-108 44.7s-79.1-15.9-108-44.7c-28.8-28.8-44.7-67.2-44.7-108s15.9-79.1 44.7-108c28.9-28.8 67.2-44.7 108-44.7m0-15c-92.6 0-167.7 75.1-167.7 167.7s75.1 167.7 167.7 167.7S681 654.7 681 562.1s-75.1-167.7-167.7-167.7z" fill="#999999"></path><path d="M198.6 289h91.7v518h-91.7zM735.2 289h91.7v518h-91.7z" fill="#ACE2F9"></path></g></svg>
+            ),
+            text: "Thorough Diagnosis and Treatment Steps"
+        },
+        {
+            icon: (
+                <svg viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M97.8357 54.6682C177.199 59.5311 213.038 52.9891 238.043 52.9891C261.298 52.9891 272.24 129.465 262.683 152.048C253.672 173.341 100.331 174.196 93.1919 165.763C84.9363 156.008 89.7095 115.275 89.7095 101.301" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M98.3318 190.694C-10.6597 291.485 121.25 273.498 148.233 295.083" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M98.3301 190.694C99.7917 213.702 101.164 265.697 100.263 272.898" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M208.308 136.239C208.308 131.959 208.308 127.678 208.308 123.396" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M177.299 137.271C177.035 133.883 177.3 126.121 177.3 123.396" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M203.398 241.72C352.097 239.921 374.881 226.73 312.524 341.851" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M285.55 345.448C196.81 341.85 136.851 374.229 178.223 264.504" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M180.018 345.448C160.77 331.385 139.302 320.213 120.658 304.675" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M218.395 190.156C219.024 205.562 219.594 220.898 219.594 236.324" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M218.395 190.156C225.896 202.037 232.97 209.77 241.777 230.327" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M80.1174 119.041C75.5996 120.222 71.0489 119.99 66.4414 120.41" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M59.5935 109.469C59.6539 117.756 59.5918 125.915 58.9102 134.086" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M277.741 115.622C281.155 115.268 284.589 114.823 287.997 114.255" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M291.412 104.682C292.382 110.109 292.095 115.612 292.095 121.093" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M225.768 116.466C203.362 113.993 181.657 115.175 160.124 118.568" stroke="#3276c3" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            ),
+            text: "AI-Powered & Expert-Driven"
+        }
+    ];
+
     return (
-        <div className='Content_2'>
-            <div className='Content_2_left'>
-                <span className='Content_2_left_first'>Why Choose Us?</span>
-                <span className='Content_2_left_second'>SkinGuard AI is a cutting-edge platform that connects userswith dermatology experts,providing the first and only AI-powered remote skin analysis in Vietnam. Our advantages:</span>
-                <span className='Content_2_left_third'>Fast and Convenient</span>
-                <span className='Content_2_left_third'>100% Free and Always Available</span>
-                <span className='Content_2_left_third'>Thorough Diagnosis and Treatment Steps</span>
-                <span className='Content_2_left_third'>AI-Powered Diagnosis, Trusted by Experts</span>
+        <div className="Content_2">
+            <div className="Content_2_left">
+                <span className="Content_2_left_first">Why Choose Us?</span>
+                <span className="Content_2_left_second">
+                    SkinGuard AI is a cutting-edge platform that connects users with dermatology experts, providing the first and only AI-powered remote skin analysis in Vietnam. Our advantages:
+                </span>
+                <div className="Content_2_left_third_group">
+                    {features.map((feature, idx) => (
+                        <div className="icon-text" key={idx}>
+                            {feature.icon}
+                            <span className="Content_2_left_third">{feature.text}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className='Content_2_right'>
-                <img src="/src/assets/doctor_content_2.png" alt="" className='Content_2_right_img'/>
+            <div className="Content_2_right">
+                <img src="/src/assets/doctor_content_2.png" alt="Doctor" className="Content_2_right_img" />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Content_2
+export default Content_2;
