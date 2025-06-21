080d7baaf528dc1e9181d4d2711f0cb6e8005dd0
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2025-06-21 12:43:35 +0700

    Fix Backend Doctor and Consult
    Fix Backend Doctor and Consult

--- Backend/src/consult/dto/create-consult.dto.ts
+++ Backend/src/consult/dto/create-consult.dto.ts
@@ -1,7 +1,15 @@
+// export class CreateConsultDto {
+//   consult_id: string;
+//   date: Date;
+//   patient_id: string;
+//   patient_description: string;
+//   result: string;
+// }
 export class CreateConsultDto {
   consult_id: string;
   date: Date;
-  patient_id: string;
+  doctor_id: string;    // ObjectId string
+  patient_id: string;   // ObjectId string
   patient_description: string;
   result: string;
 }
--- Backend/src/doctor/entities/doctor.entity.ts
+++ Backend/src/doctor/entities/doctor.entity.ts
@@ -1,3 +1,53 @@
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { Document, Types } from 'mongoose';
+
+// export type DoctorDocument = Doctor & Document;
+
+// @Schema({ timestamps: true })
+// export class Doctor {
+//   @Prop({ required: true, unique: true })
+//   doctor_id: string;
+
+//   @Prop({ required: true })
+//   password: string;
+//   @Prop({ required: true })
+//   fullName: string;
+
+//   @Prop({ required: true })
+//   specialty: string;
+
+//   @Prop()
+//   bio: string;
+
+//   @Prop({ required: true })
+//   phone: string;
+
+//   @Prop()
+//   avatar: string;
+
+//   @Prop({ type: Number, min: 1, max: 5 })
+//   rating: number;
+
+//   @Prop({ required: true, unique: true })
+//   email: string;
+
+//   @Prop()
+//   token: string;
+
+//   @Prop({ default: 'active' })
+//   status: string;
+
+//   @Prop()
+//   deletedAt: Date;
+
+//   @Prop()
+//   experienceYears: number;
+
+//   @Prop({ default: false })
+//   deleted: boolean;
+// }
+
+// export const DoctorSchema = SchemaFactory.createForClass(Doctor);
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
@@ -10,41 +60,30 @@ export class Doctor {
 
   @Prop({ required: true })
   password: string;
+
   @Prop({ required: true })
   fullName: string;
 
   @Prop({ required: true })
-  specialty: string;
-
-  @Prop()
-  bio: string;
-
-  @Prop({ required: true })
-  phone: string;
-
-  @Prop()
-  avatar: string;
+  discipline: string;
 
   @Prop({ type: Number, min: 1, max: 5 })
   rating: number;
 
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Consult' }], default: [] })
+  consult_list_id: Types.ObjectId[];
+
+  @Prop()
+  avatar: string;
+
+  @Prop()
+  permission: string;
+
   @Prop({ required: true, unique: true })
   email: string;
 
-  @Prop()
-  token: string;
-
-  @Prop({ default: 'active' })
-  status: string;
-
-  @Prop()
-  deletedAt: Date;
-
-  @Prop()
-  experienceYears: number;
-
-  @Prop({ default: false })
-  deleted: boolean;
+  @Prop({ required: true })
+  phoneNumber: string;
 }
 
 export const DoctorSchema = SchemaFactory.createForClass(Doctor);
